0552de6cbce061ad6488c847b7c3eac4c10ca0f7
Rohan <rairohan2309@gmail.com>
2024-07-01 12:29:22 +0530

    first cloud deploy

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')
 const ExpressError=require('./util/ExpressError.js')
 const session = require('express-session')
+const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
 const passport=require("passport")
 const localStrategy= require("passport-local")
@@ -27,6 +28,9 @@ app.use(express.static(path.join(__dirname,"/public")));
 app.use(methodOverride('_method'))
 app.engine('ejs', ejsMate);
 
+// const mongo_url='mongodb://127.0.0.1:27017/wonderlust';
+const dbUrl= process.env.ATLASDB_URL;
+
 main().then(res=>{
     console.log("database connected") 
 }).catch(err=>{
@@ -36,11 +40,25 @@ main().then(res=>{
 
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/wonderlust');
+  await mongoose.connect(dbUrl);
 }
 
+const store = MongoStore.create({
+    mongoUrl:dbUrl,
+    crypto:{
+        secret:process.env.SECRET,
+    },
+    touchAfter: 24 * 3600,
+
+})
+
+store.on("error", ()=>{
+    console.log("ERROR In MONGO SESSION STORE", err)
+});
+
 const sessionOptions={
-    secret : "mySuperSecretCode",
+    store,
+    secret : process.env.SECRET,
     resave : false,
     saveUninitialized:true,
     cookie:{
@@ -49,6 +67,8 @@ const sessionOptions={
         httpOnly: true,
     },
 }
+
+
 
 app.use(session(sessionOptions));
 app.use(flash());
